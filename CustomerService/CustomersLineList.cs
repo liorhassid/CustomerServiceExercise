c1852f7d75cd8b8dbdfb27094f1f48ed5e94b76f
liorhassid <lior.hassid@gmail.com>
2024-09-04 15:26:47 +0300

    added a List Implementation of customers line

---
+++ CustomerService/CustomersLineList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerService
+{
+    public class CustomersLineList : ICustomersLine
+    {
+        private List<Customer> CustomersList;
+
+        public CustomersLineList()
+        {
+            CustomersList = new List<Customer>();
+        }
+
+        public void AddToLine(Customer customer)
+        {
+            CustomersList.Add(customer);
+        }
+
+        public int Length()
+        {
+            return CustomersList.Count;
+        }
+
+        public Customer RemoveFromLine()
+        {
+            Customer customer = CustomersList[0];
+            CustomersList.RemoveAt(0);
+            return customer;
+        }
+    }
+}
