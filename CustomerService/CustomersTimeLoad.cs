5d1b1b69eb8d372ed3b9df439ea179a688dbda6a
liorhassid <lior.hassid@gmail.com>
2024-09-04 15:51:38 +0300

    changed Time Load File Logic

--- CustomerService/CustomersTimeLoad.cs
+++ CustomerService/CustomersTimeLoad.cs
@@ -12,6 +12,11 @@ namespace CustomerService
 
         public CustomersTimeLoad(ICustomersLine newLine, int hour, int minute) : base(newLine)
         {
+            SetCurrentTime(hour, minute);
+        }
+
+        public void SetCurrentTime(int hour, int minute)
+        {
             CurrentTime = new TimeOnly(hour, minute);
             if (CurrentTime.IsBetween(new TimeOnly(Constants.PrimeTimeStartHour, 0),
                 new TimeOnly(Constants.PrimeTimeEndHour, 0)))
