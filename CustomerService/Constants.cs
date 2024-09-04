bbf548a433279c487f51f8b7fda58946e007a875
liorhassid <lior.hassid@gmail.com>
2024-09-04 15:09:38 +0300

    added Time Load implementation to customers service

--- CustomerService/Constants.cs
+++ CustomerService/Constants.cs
@@ -11,5 +11,13 @@ namespace CustomerService
         public const int CustomersInLinePerLoadIndex = 5;
 
         public const int MaxLoadIndex = 10;
+
+        public const int PrimeTimeLoadIndex = 8;
+
+        public const int ChillTimeLoadIndex = 2;
+
+        public const int PrimeTimeStartHour = 8;
+
+        public const int PrimeTimeEndHour = 18;
     }
 }
---
+++ CustomerService/CustomersTimeLoad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerService
+{
+    public class CustomersTimeLoad : CustomerService
+    {
+        private TimeOnly CurrentTime {  get; set; }
+
+        public CustomersTimeLoad(ICustomersLine newLine, int hour, int minute) : base(newLine)
+        {
+            CurrentTime = new TimeOnly(hour, minute);
+            if (CurrentTime.IsBetween(new TimeOnly(Constants.PrimeTimeStartHour, 0),
+                new TimeOnly(Constants.PrimeTimeEndHour, 0)))
+                LoadIndex = Constants.PrimeTimeLoadIndex;
+            else LoadIndex = Constants.ChillTimeLoadIndex;
+        }
+    }
+}
