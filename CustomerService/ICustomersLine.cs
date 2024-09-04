215d7397092b523f4caf6b4961c2fb4b1a1198f4
liorhassid <lior.hassid@gmail.com>
2024-09-04 14:23:25 +0300

    added Customers Line Interface

---
+++ CustomerService/ICustomersLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerService
+{
+    public interface ICustomersLine
+    {
+        void AddToLine(Customer customer);
+
+        Customer RemoveFromLine();
+    }
+}
